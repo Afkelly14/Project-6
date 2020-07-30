e65a4878782b9b31a75e7622c99c8e70da2649f5
Alex_Kelly <afkelly14@Gmail.com>
2020-07-29 21:53:38 -0400

    creating CRUD methods and routes

--- index.js
+++ index.js
@@ -3,23 +3,39 @@ const Drinks = require("./models/Drinks");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
+//**need to check all of the below and add more CRUD */
 
 //show all the drinks
-app.get('/drinks', (req, resp) => {
-    Drinks.find({}).then((drinks) => {
-        resp.json(drinks);
-    });
+app.get("/drinks", (req, resp) => {
+  Drinks.find({}).then((drink) => {
+    resp.json(drink);
+  });
+});
+
+//find by the id
+app.get("/drinks/:id", (req, resp) => {
+  Drinks.findById(req.params.id).then((drink) => {
+    resp.json(drink);
+  });
+});
+
+//request so the user could POST a new drink
+app.post("/drink", (req, resp) => {
+  Drinks.create(req.body).then((drink) => {
+    resp.json(drink);
+  });
 });
 
 //show the type of glasses
-app.get('/')
-
+app.get("/drinks/glass", (req, resp) => {
+  Drinks.find({}).then((drinks) => {
+    resp.json(drinks);
+  });
+});
 
 //create a show (detail) action to get the details of a PARTICULAR list
 //find drinks by their ingredient
 
-
-
 app.listen(3000, () => {
-    console.log("this is working....");
+  console.log("this is working....");
 }); //app
--- models/Drinks.js
+++ models/Drinks.js
@@ -1,19 +1,6 @@
 const mongoose = require("../db/connection.js");
 const Schema = mongoose.Schema;
 
-// const ingredientSchema = new Schema({
-//   strIngredient1: String,
-//   strIngredient2: String,
-//   strIngredient3: String,
-//   strIngredient4: String,
-//   strIngredient5: String,
-//   strIngredient6: String,
-//   strIngredient7: String,
-//   strIngredient8: String,
-//   strIngredient9: String,
-//   strIngredient10: String
-// });
-
 const drinkSchema = new Schema({
   idDrink: String,
   strCategory: String,
